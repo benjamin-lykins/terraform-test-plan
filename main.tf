b77a4f4ff9a3b376ff264a053718a9052671c79a
Ben Lykins <benjamin.m.lykins@gmail.com>
2023-08-30 16:51:08 -0400

    adding random id resource

--- main.tf
+++ main.tf
@@ -1,20 +1,32 @@
 terraform {
   required_providers {
     dns = {
-      source = "hashicorp/dns"
+      source  = "hashicorp/dns"
       version = "3.3.2"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
   }
-}
-
-provider "dns" {
-  # Configuration options
 }
 
 data "dns_a_record_set" "google" {
   host = "google.com"
 }
 
+//this resource will always recreate. 
+resource "random_id" "rng" {
+  keepers = {
+    first = "${timestamp()}"
+  }     
+  byte_length = 8
+}
+
 output "google_addrs" {
   value = join(",", data.dns_a_record_set.google.addrs)
+}
+
+output "random_id" {
+  value = random_id.rng.id
 }
