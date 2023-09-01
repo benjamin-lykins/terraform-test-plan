377f8b58567d57c512b1ac9602bc6fe94f0e1ea3
Ben Lykins <benjamin.m.lykins@gmail.com>
2023-09-01 10:51:05 -0400

    unformatting

--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
 terraform {
-  required_providers {
-    dns = {
-      source  = "hashicorp/dns"
-      version = "3.3.2"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
-  }
+required_providers {
+dns = {
+source  = "hashicorp/dns"
+version = "3.3.2"
+}
+random = {
+source  = "hashicorp/random"
+version = "3.5.1"
+}
+}
 }
 
 data "dns_a_record_set" "google" {
-  host = "google.com"
+host = "google.com"
 }
 
 //this resource will always recreate. 
 resource "random_id" "rng" {
-  keepers = {
-    first = "${timestamp()}"
-  }     
-  byte_length = 8
+keepers = {
+first = "${timestamp()}"
+}     
+byte_length = 8
 }
 
 output "google_addrs" {
-  value = join(",", data.dns_a_record_set.google.addrs)
+value = join(",", data.dns_a_record_set.google.addrs)
 }
 
 output "random_id" {
-  value = random_id.rng.id
+value = random_id.rng.id
 }
